382153684683ae317e6ea25e2c0f63ce345314a6
chandoithatsu <100838893+Afdfghjhgcfghvh@users.noreply.github.com>
2022-04-22 16:45:34 +0700

    h

--- src/CacThiSinhThiDH/Chay.java
+++ src/CacThiSinhThiDH/Chay.java
@@ -1,5 +1,7 @@
 package CacThiSinhThiDH;
 
+import java.util.Scanner;
+
 public class Chay {
     public static void main(String[] args) {
         phuongThucQuanLy phuongThucQuanLy1=new phuongThucQuanLy();
@@ -8,6 +10,18 @@ public class Chay {
         phuongThucQuanLy1.themMoiThiSinh(new QuanLyThiSinhDuThi(125,"nguyen van c","hn3",5));
         phuongThucQuanLy1.themMoiThiSinh(new QuanLyThiSinhDuThi(126,"nguyen van d","hn4",3));
         phuongThucQuanLy1.htThongTinThiSinhvaKhoithi();
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("nhập SBD cần tìm :");
+        int soBaoDanh = sc.nextInt();
+        System.out.println(phuongThucQuanLy1.timKiemTheoSBD(soBaoDanh));
+
+
+
+        System.out.println(" nhập 0 để thoát chương trình");
+        sc.nextInt();
+        System.out.println(phuongThucQuanLy1.thoatChuongTrinh());
+
     }
 
 
